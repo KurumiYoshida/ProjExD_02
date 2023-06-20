ea1a35ec3112c19f8304acafe32f126b1757181a
kuru <c0a22148e1@edu.teu.ac.jp>
2023-06-20 15:30:29 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -55,6 +55,10 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
+        if kk_rct.colliderect(bd_rct):  # 練習５
+            print("ゲームオーバー")
+            return   # ゲームオーバー
+            
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]  # 合計移動量
         for k, mv in delta.items():
